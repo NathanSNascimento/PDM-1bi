b6fe45a391d05a0340c88b71fbd6db2a1621fccc
NathaNascimento <nathansantos3211@gmail.com>
2019-04-09 21:50:51 -0300

    no message

--- app/src/main/java/com/example/churrascometro/ResultadoActivity.kt
+++ app/src/main/java/com/example/churrascometro/ResultadoActivity.kt
@@ -2,14 +2,57 @@ package com.example.churrascometro
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
+
+    var picanha: Double = 0.0
+    var maminha: Double = 0.0
+    var linguica: Double = 0.0
+    var frango: Double = 0.0
+    var arroz: Double = 0.0
+    var batata: Double = 0.0
+    var farofa: Double = 0.0
+    var paes: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
-        val dados: Bundle = intent.extras
-        val qtdHomem = dados.getInt("numero")
+        val dados :Bundle = intent.extras
+
+        var homens = dados.getString("mens").toInt()
+        var mulheres = dados.getString("womans").toInt()
+        var criancas = dados.getString("kids").toInt()
+
+        somaResultado(homens, mulheres, criancas)
+
+    }
+
+    fun somaResultado(homens: Int, mulheres: Int, criancas: Int) {
+
+        picanha = (homens * 240.0) + (mulheres * 180.0) + (criancas * 120.0)
+        maminha = (homens * 80.0) + (mulheres * 60.0) + (criancas * 40.0)
+        linguica = (homens * 40.0) + (mulheres * 30.0) + (criancas * 20.0)
+        frango = (homens * 40.0) + (mulheres * 30.0) + (criancas * 20.0)
+        arroz = (homens * 100.0) + (mulheres * 80.0)
+        batata = (homens * 200.0) + (mulheres * 150.0) + (criancas * 100.0)
+        farofa = (homens * 100.0) + (mulheres * 80.0)
+        paes = (homens * 1.5) + (mulheres * 1.5)
+
+        text_Picanha.setText(picanha.toString())
+        text_Maminha.setText(maminha.toString())
+        text_lingui.setText(linguica.toString())
+        text_frango.setText(frango.toString())
+        text_Arroz.setText(arroz.toString())
+        text_batata.setText(batata.toString())
+        text_farofa.setText(farofa.toString())
+        text_paes.setText(paes.toString())
+
+        button_voltar.setOnClickListener {
+            finish()
+
+        }
+
     }
 }
